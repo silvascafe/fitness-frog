9b6a271baebb4351c05d0db0776f52e20b644ccb
Windesson Silva Almeida <windesson.silvaalmeida@boeing.com>
2019-01-22 07:38:45 -0500

    Updated th Add action method to capture POST data

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -48,9 +48,20 @@ namespace Treehouse.FitnessFrog.Controllers
            return View();
         }
 
-        [ActionName("Add"), HttpPost]
-        public ActionResult AddPost()
+        //[ActionName("Add"), HttpPost]
+        //public ActionResult AddPost(){
+        [HttpPost]
+        public ActionResult Add(string date, string activityId, string duration,
+            string intensity,string exclude, string notes)
         {
+            //string data = Request.Form["Date"];
+            ViewBag.Date = date;
+            ViewBag.ActivityId = activityId;
+            ViewBag.Duration = duration;
+            ViewBag.Intensity = intensity;
+            ViewBag.Exclude = exclude;
+            ViewBag.Notes = notes;
+
             return View();
         }
 

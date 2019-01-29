e9562f897eaaf0c27639db6a5adf226adbd4b40f
Windesson Silva Almeida <windesson.silvaalmeida@boeing.com>
2019-01-29 18:01:33 -0500

    Completed the "Edit Entry" page

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -102,6 +102,7 @@ namespace Treehouse.FitnessFrog.Controllers
                return RedirectToAction("Index");
             }
 
+            SetupActivitiesSelectListItems();
             return View(entry);
         }
 
